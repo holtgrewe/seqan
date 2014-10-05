429f8925ab245c37fd3c2822627560e2b6d125fb
Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
2014-10-05 03:11:50 +0200

    Update coveralls.sh

--- util/travis/coveralls.sh
+++ util/travis/coveralls.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Note that this only works if the tests were built and linked using the
-# profiling flags.
-if [ "$CXX" == "g++" ];
-then
+# --coverage flags.
+if [ "$CXX" == "clang++" ]; then
   sudo pip install cpp-coveralls
   # redirect to /dev/null to stay below 4MB of stderr/stdout output
-  coveralls -b _build -r _build -e core/tests -e extras/tests -t ${COVERALLS_TOKEN} &>/dev/null
+  coveralls -b _build --gcov $(which llvm-cov) -r _build -e core/tests -e extras/tests -t ${COVERALLS_TOKEN} &>/dev/null
 fi
