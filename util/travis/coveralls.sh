1ae9f3e664e7bd66993d06c9ed3ccfdb995d429d
Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
2014-10-05 16:31:57 +0200

    [INTERNAL] Allowing coveralls output to stderr

--- util/travis/coveralls.sh
+++ util/travis/coveralls.sh
@@ -6,5 +6,5 @@ if [ "$CXX" == "g++" ];
 then
   sudo pip install cpp-coveralls
   # redirect to /dev/null to stay below 4MB stdout
-  coveralls -b . -r . -E '.*CMake.*\.cpp' -E '.*CMake.*\.c' -t "${COVERALLS_TOKEN}" &>/dev/null
+  coveralls -b . -r . -E '.*CMake.*\.cpp' -E '.*CMake.*\.c' -t "${COVERALLS_TOKEN}" >/dev/null
 fi
