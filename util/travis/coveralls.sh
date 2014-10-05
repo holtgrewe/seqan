afa4495e3106558230e6a14be2020810fcdc6dcf
Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
2014-10-05 03:02:34 +0200

    [FIX,INTERNAL] Update to coveralls.sh

--- util/travis/coveralls.sh
+++ util/travis/coveralls.sh
@@ -5,5 +5,6 @@
 if [ "$CXX" == "g++" ];
 then
   sudo pip install cpp-coveralls
-  coveralls -b _build -r _build -e core/tests -e extras/tests -t ${COVERALLS_TOKEN}
+  # redirect to /dev/null to stay below 4MB of stderr/stdout output
+  coveralls -b _build -r _build -e core/tests -e extras/tests -t ${COVERALLS_TOKEN} &>/dev/null
 fi
